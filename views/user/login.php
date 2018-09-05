5cf457ef65df13f0cca0b0808892c8938ff82157
andrii <kaa89pl@gmail.com>
2018-09-05 10:15:41 +0300

    describe join login page

--- views/user/login.php
+++ views/user/login.php
@@ -1,1 +1,15 @@
-<h1>Log in</h1>
+
+<div class="panel panel-warning">
+
+    <div class="panel-heading">
+        <h1>Log in</h1>
+    </div>
+    <div class="panel-body">
+
+        email<br/>
+        password<br/>
+
+        [Create]
+    </div>
+
+</div>
