25c9a2f8a5d327008c0a8b90430d10e98461147c
andrii <kaa89pl@gmail.com>
2018-09-10 14:48:18 +0300

    db conection

--- config/console.php
+++ config/console.php
@@ -4,13 +4,6 @@ return [
     'id' => 'safronov-console',
     'basePath' => dirname(__DIR__),
     'components' => [
-        'db' =>  [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=safronov',
-            'username' => 'andrii',
-            'password' => '1',
-            'charset' => 'utf8',
-            'tablePrefix' => 'yii_'
-        ],
+        'db' =>  require (__DIR__.'/db.php')
         ]
 ];
---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=safronov',
+    'username' => 'andrii',
+    'password' => '1',
+    'charset' => 'utf8',
+    'tablePrefix' => 'yii_'
+];
