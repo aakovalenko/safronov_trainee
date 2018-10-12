897dacca1eb226a1d684699d7e216c5b4b5803fc
andrii <kaa89pl@gmail.com>
2018-10-12 10:38:42 +0300

    add userJoinForm validation rules

--- models/UserJoinForm.php
+++ models/UserJoinForm.php
@@ -9,4 +9,19 @@ class UserJoinForm extends Model
     public $email;
     public $password;
     public $password2;
+
+    public function rules ()
+    {
+        return [
+          ['name','required'],
+          ['email', 'required'],
+            ['password', 'required'],
+            ['password2', 'required'],
+            ['name','string','min' => 3,'max' => 30],
+            ['email','email', 'message'=>'Адрес почты указан неверно'],
+            ['password','string','min'=>4],
+            ['password2', 'compare','compareAttribute' => 'password']
+
+        ];
+    }
 }
